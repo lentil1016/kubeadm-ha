ae5d64c8d192eb1a8b96665598b3f200de824ed9
Cruse Lee <cruselee1@live.cn>
2018-12-11 16:57:21 +0800

    Update kubeha-gen.sh
    fix typo.

--- kubeha-gen.sh
+++ kubeha-gen.sh
@@ -48,13 +48,13 @@ cluster-info:
   Net Interface:    ${NET_IF}
   CIDR:             ${CIDR}
 """
-echo -n 'Please print "yes" to continue or "no" to cancle: '
+echo -n 'Please print "yes" to continue or "no" to cancel: '
 read AGREE
 while [ "${AGREE}" != "yes" ]; do
 	if [ "${AGREE}" == "no" ]; then
 		exit 0;
 	else
-		echo -n 'Please print "yes" to continue or "no" to cancle: '
+		echo -n 'Please print "yes" to continue or "no" to cancel: '
 		read AGREE
 	fi
 done
